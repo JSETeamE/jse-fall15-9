eec5fbeb9f1b396ba0d6c5a8faf765fb62749809
lboom <tishbite@hobotech.org>
2015-09-20 18:06:37 -0700

    need TaskView and CreateTasksView

--- views.js
+++ views.js
@@ -5,18 +5,31 @@ var GUI = (function(){ //IIFE for all Views
     });
     
     var CreateTaskView = Backbone.View.extend({
-    
+
     });
     var UnassignedTasksView = Backbone.View.extend({
         render: function() {
-            // XXX not done
             var newTaskBtn = '<button id="newTask"> Create new Task </button>';
-            this.$el.html('<div>' + newTaskBtn + '</div>');
+            this.$el.html(newTaskBtn);
         },
         events: {
-            "click #newTask" : "CreateTaskView"
+            "click #newTask" : "newTask"
+        },
+        // helper function
+        newTask: function() {
+            // create a new task
+            var newIssue = new IssueModel({
+                // XXX this is not tested
+                // grab values from input fields in CreateTasksViews
+                title: '',
+                description: '',
+                creator: '', // this is person logged in?
+                assignee: '', // dropdown choice
+            });
+
+            // XXX testing
+            console.log('I work!');
         }
-    
     });
     var UserTasksView = Backbone.View.extend({
         render: function() {
@@ -24,18 +37,22 @@ var GUI = (function(){ //IIFE for all Views
         },
     
     });
-    
     var UserView = Backbone.View.extend({
-    
+        el: '#taskZone',
+        render: function() {
+            var unassigned = new UnassignedTasksView();
+            unassigned.render();
+            this.$el.html(unassigned.$el);
+        }
     });
     
     var LoginView = Backbone.View.extend({
-        el: '#loginView',
+        el: '#taskZone',
         render: function(){
-        var html = 'Select a User:<select id=users>';
-        this.collection.each(function(model){
-        var username = model.attributes.username;
-        html += '<option value=' + username + '>' + username + '</option>';
+            var html = 'Select a User:<select id=users>';
+            this.collection.each(function(model){
+            var username = model.attributes.username;
+            html += '<option value=' + username + '>' + username + '</option>';
         });
 
         html += '</select><br /><button id=login>Login</button>';
@@ -48,12 +65,13 @@ var GUI = (function(){ //IIFE for all Views
     },
     events: {       
         'click #login': 'login'
-       
     },
     login: function(){
-
         alert('You are logged in as: ' + $('#users', this.$el).val());
-      }  
+        // replace login with userView
+        var user = new UserView();
+        user.render();
+    }  
     });
     
     
